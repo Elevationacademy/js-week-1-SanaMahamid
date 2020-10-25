b88f9bbc84821908c5d9020c779d3fb8b1aa619f
sana mahamid <sanaa_6-5.7@hotmail.com>
2020-10-25 18:41:42 +0200

    arrays learn

---
+++ JS-Arrays/main.js
@@ -0,0 +1,22 @@
+
+
+/*let myStuff = ['laptop', 'iphone', 'notebook']
+console.log(myStuff[1])
+myStuff.push(myStuff[0])
+myStuff.unshift(myStuff[2])
+console.log(myStuff)
+myStuff.push(... myStuff)
+console.log(myStuff)
+myStuff.splice(0,1)
+let array = ["clouds", "sky", "birds", "worms", "earth", "dirt", "clothes", "human", "life"]
+array.splice(0,array.length-1)
+console.log(array)
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(2, 0, "Lemon", "Kiwi");
+console.log(fruits)
+
+
+const plants = ["Oak", "Lilly", "Belladonna", "Tulip", "Yew"]
+
+let lastIndex = plants.length - 1 // equal to (5 - 1) = 4
+console.log("The first plant is " + plants[0] + ", and the last one is " + plants[lastIndex])*/
